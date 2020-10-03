01e0ab6d38589ee29638c2c65ea963ce831e507d
frideno <4drive13@gmail.com>
2020-10-03 16:57:39 +0300

    tmp version which is google search

--- popup.js
+++ popup.js
@@ -9,3 +9,28 @@ slider.oninput = function() {
 }
 
 // functions that create the typing logic itself.
+// myURLs contains the websites where you want your content script to run
+const myURLs = ['google.com'];
+
+var startTyping = function() {
+    // on google search bar.
+    inp = document.getElementsByTagName('input')[4];
+    inp.value = 'trytry123';
+    inp.focus();
+
+}
+
+var start = function() {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+        let url = tabs[0].url;
+        console.log(url);
+        if (myURLs.some(allowed_url => url.includes(allowed_url))) {
+            chrome.tabs.executeScript(null, { code: "var startTyping = " + startTyping.toString() + ";startTyping();" }, function(result) {});
+            window.close()
+        }
+    });
+
+}
+
+var startButton = document.getElementById("startTypingButton");
+startButton.addEventListener('click', start)
