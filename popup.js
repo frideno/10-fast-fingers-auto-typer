c7652a43a3b4fa7bb52bd7aa05977c2795b037cc
frideno <4drive13@gmail.com>
2020-10-03 15:59:33 +0300

    enable jquery

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+// slider setup.
+
 var slider = document.getElementById("typingSpeedRangeSlider");
 var output = document.getElementById("typingSpeedValue");
 output.innerHTML = slider.value;
@@ -5,3 +7,5 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
     output.innerHTML = this.value;
 }
+
+// functions that create the typing logic itself.
